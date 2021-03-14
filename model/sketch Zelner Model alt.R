f372c3ae7282c1153e2993c497abdf431eba3f86
Weinberger <daniel.weinberger@yale.edu>
2021-03-14 14:40:17 +0100

    individual-level indexing

--- model/sketch Zelner Model alt.R
+++ model/sketch Zelner Model alt.R
@@ -2,14 +2,12 @@
 zelner_jags2 <- "
 model{
 for(i in 1:N.HH){
-
-
-    
      for(j in 1:N.hh.members[i]){
      
         for(t in 1:tmax[i]){
-          log_prob_no_inf_t[i,j,t] <- log( 1 - (beta[i,j] * I[i,t] + alpha[i,j]) )
+          log_prob_no_inf_t[i,j,t] <- log( 1 - (beta[i,j] * sum_I[i,t] + alpha[i,j]) )
         }
+        
         y[i,j] ~ dbern(q[i,j]) #y=UNinfected matrix
      
      ##NOTE THESE ARE PROBABLY NOT RIGHT--PROB NEED TO FLIP SOME OF THEM AROUND
@@ -17,7 +15,7 @@ for(i in 1:N.HH){
       
       prob_no_inf_inf_person[i,j] <- exp(sum(log_prob_no_inf_t[i,j,1:((day.matrix[i,j] - 1)) ])) #P no infections prior to infection
       
-      prob_inf[i,j] <- infected_matrix[i,j] * (beta[i,j] * I[i,day.matrix[i,j]] + alpha[i,j]) #for infected people only
+      prob_inf[i,j] <- infected_matrix[i,j] * (beta[i,j] * sum_I[i,day.matrix[i,j]] + alpha[i,j]) #for infected people only
 
     q[i,j] <- (1 - prob_no_inf_inf_person[i,j]) * (1 - prob_inf[i,j]) * infected_matrix[i,j] + #prob for infection at time t and not before
                     (1 - prob_no_inf_uninf[i,j]) * (1- infected_matrix[i,j] ) + 1e-6 #prob for uninfected peopel    
@@ -29,49 +27,59 @@ for(i in 1:N.HH){
 ##### Attaching latent piece
 
 for(i in 1:N.HH){
-    Dur.Inf[i] ~ dgamma(sh1, ra1) #duration infectiusness
-    Dur.Latent[i] ~dgamma(sh2, ra2) #Duration latent
-
-    delta[i] <- 1/Dur.Inf[i]
-    epsilon[i] <- 1/Dur.Latent[i]
-    
-    S[i,1] <- N.hh.members[i] - 1
-    E[i,1] <- 0
-    I[i,1] <- 1
-    R[i,1] <- 0
-    NewI[i,1] <- 0
-    CumNewI[i,1] <- 0
-    dS[i,1] <-0
-    dE[i,1] <-0
-    dI[i,1] <-0
-    dR[i,1] <-0
-
-    for(t in 2:43){
-      dS[i,t] <- -S[i,(t-1)]*(beta * I[i,(t-1)] + alpha)
-      dE[i,t] <- S[i,(t-1)]*(beta * I[i,(t-1)] + alpha) - E[i, (t-1)]*delta[i]
-      dI[i,t] <- S[i,(t-1)]*(beta * I[i,(t-1)] + alpha) - I[i,(t-1)] * epsilon[i]
-      dR[i,t] <- I[i,(t-1)] * epsilon[i]
-      
-      S[i,t] <- dS[i,t] + S[i,(t-1)]
-      E[i,t] <- dE[i,t] + E[i,(t-1)]
-      I[i,t] <- dI[i,t] + I[i,(t-1)]
-      R[i,t] <- dR[i,t] + R[i,(t-1)]
-      NewI[i,t] <- S[i,(t-1)]*(beta * I[i,(t-1)] + alpha)
-      CumNewI[i,t] <- NewI[i,(t-1)] + NewI[i,t]
-    }
-    SecondaryI[i] <- sum(NewI[i,])
-}
-
-for(i in 1:N.HH){
      for(j in 1:N.hh.members[i]){
-     
+
+ 
      alpha[i,j] <- exp(log.alpha[i,j])
      beta[i,j]  <- exp(log.beta[i,j])
      log.alpha[i,j] ~ dnorm(mu1,tau1)
      log.beta[i,j] ~ dnorm(mu2,tau2)
      
+    Dur.Inf[i,j] ~ dgamma(sh1, ra1) #duration infectiusness
+    Dur.Latent[i,j] ~dgamma(sh2, ra2) #Duration latent
+
+    delta[i,j] <- 1/Dur.Inf[i,j]
+    epsilon[i,j] <- 1/Dur.Latent[i,j]
+    
+    ##In this formulation, S,E,I,R are defined by individual; sum_S, sum_E, sum_I are defined by household
+    S[i,j,1] <- (N.hh.members[i] - 1)/N.hh.members[i]
+    E[i,j,1] <- 0
+    I[i,j,1] <- 1/N.hh.members[i]
+    R[i,j,1] <- 0
+    NewI[i,j,1] <- 0
+    CumNewI[i,j,1] <- 0
+    dS[i,j,1] <-0
+    dE[i,j,1] <-0
+    dI[i,j,1] <-0
+    dR[i,j,1] <-0
+
+    for(t in 2:43){
+      dS[i,j,t] <- (-sum_S[i,j,(t-1)]*(beta[i,j] * sum_I[i,j,(t-1)] + alpha[i,j]) )/N.hh.members[i]
+      dE[i,j,t] <- (sum_S[i,j,(t-1)]*(beta[i,j] * sum_I[i,j,(t-1)] + alpha[i,j]) - sum_E[i, j,(t-1)]*delta[i,j])/N.hh.members[i]
+      dI[i,j,t] <- (sum_S[i,j,(t-1)]*(beta[i,j] * sum_I[i,(t-1)] + alpha[i,j]) - sum_I[i,(t-1)] * epsilon[i,j])/N.hh.members[i]
+      dR[i,j,t] <- (sum_I[i,j,(t-1)] * epsilon[i,j])/N.hh.members[i]
+      
+      S[i,j,t] <- dS[i,j,t] + S[i,j,(t-1)]
+      E[i,j,t] <- dE[i,j,t] + E[i,j,(t-1)]
+      I[i,j,t] <- dI[i,j,t] + I[i,j,(t-1)]
+      R[i,j,t] <- dR[i,j,t] + R[i,j,(t-1)]
+      NewI[i,j,t] <- S[i,j,(t-1)]*(beta[i,j] * I[i,j,(t-1)] + alpha[i,j])
+      CumNewI[i,j,t] <- NewI[i,j,(t-1)] + NewI[i,j,t]
+    }
+    SecondaryI[i,,] <- sum(NewI[i,,])
      }
-  }
+     
+   for(t in 1:43){
+     sum_S[i,t] <- sum(S[i,,t])
+     sum_E[i,t] <- sum(E[i,,t])
+     sum_I[i,t] <- sum(I[i,,t])
+     sum_R[i,t] <- sum(R[i,,t])
+
+   }
+
+}
+
+
 
 mu1 ~dnorm(0,1e-4)
 mu2 ~dnorm(0,1e-4)

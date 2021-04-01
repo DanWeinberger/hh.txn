a596a501377c2a01c8ac41b6bbd10d840c7b172c
ottiP <ottavia.prunas@edu.unito.it>
2021-04-01 14:35:40 -0400

    Add back bernoulli

--- model/chain_binomial_test_separate.R
+++ model/chain_binomial_test_separate.R
@@ -13,7 +13,7 @@ for(i in 1:N.HH){
 #k.last[i] <- N.hh.members[i] + 1
   for(j in 1:N.hh.members[i]){ 
 
-   # y[i,j] ~ dbern(q[i,j]) #(Person j in HH i: 1= uninfected; 0= infected)
+    y[i,j] ~ dbern(like[i,j]) #(Person j in HH i: 1= uninfected; 0= infected)
 
     for(t in 1:time.study.HH[i, selecter1]){
         log.p[i,j,(N.hh.members[i]+1),t] <- delta0 + beta1*vaxdose1[i,j]  #outside HH infection
@@ -38,7 +38,7 @@ for(i in 1:N.HH){
     prob_uninf[i,j] <- exp(sum(log.prob_uninf[i,j,1:N.hh.members[i],1:time.study.HH[i,selecter1]]))
     prob_inf_timej[i,j] <- exp(sum(log.prob_inf_timej[i,j,1:N.hh.members[i]+1]))
     prob_inf[i,j] <- prob_uninf_to_timej[i,j]* prob_inf_timej[i,j]
-    like[i,j] <- y[i,j]*prob_uninf[i,j] + (1-y[i,j])*prob_inf[i,j]
+    like[i,j] <- y2[i,j]*prob_uninf[i,j] + (1-y2[i,j])*prob_inf[i,j]+ 1e-6  
   }  
 }
 

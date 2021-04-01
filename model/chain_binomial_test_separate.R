95bc91249bb86e2f247086a6949fd9a7d88178a4
Weinberger <daniel.weinberger@yale.edu>
2021-04-01 17:38:54 +0200

    invalid range still

--- model/chain_binomial_test_separate.R
+++ model/chain_binomial_test_separate.R
@@ -2,16 +2,18 @@ chain_binomial_jags <- "
 model{
 
 #select random draw from the delay distributions
-selecter ~ dunif(1,10000) 
-selecter1 <- round(selecter)
+#selecter ~ dunif(1,10000) 
+#selecter1 ~ dround(selecter)
+
+#selecter1 <- 1
 
 #### Part 2: Likelihood definition
 
 for(i in 1:N.HH){ 
-k.last[i] <- N.hh.members[i] + 1
+#k.last[i] <- N.hh.members[i] + 1
   for(j in 1:N.hh.members[i]){ 
 
-    #y[i,j] ~ dbern(q[i,j]) #(Person j in HH i: 1= uninfected; 0= infected)
+   # y[i,j] ~ dbern(q[i,j]) #(Person j in HH i: 1= uninfected; 0= infected)
 
     for(t in 1:time.study.HH[i, selecter1]){
         log.p[i,j,(N.hh.members[i]+1),t] <- delta0 + beta1*vaxdose1[i,j]  #outside HH infection
@@ -19,17 +21,18 @@ k.last[i] <- N.hh.members[i] + 1
         d[i,j,k,t] <- step(abs(j-k)-0.5)*step( t > day.infectious[i,k,selecter1]+0.5)*step( t < min(day.exposed[i,j,selecter1], day.infectious.end[i,k,selecter1]) + 0.5)
         log.prob_uninf[i,j,k,t] <- step(abs(j-k)-0.5)*log((1-p[i,j,k,t])^(d[i,j,k,t]))
         log.p[i,j,k,t] <- alpha0 + beta1*vaxdose1[i,j]  + kappa1*vaxdose1[i,k] 
+        p[i,j,k,t] <- exp(log.p[i,j,k,t])
+
       }
        
-      d[i,j,k.last[i],t] <- step( t > day.infectious[i,k.last[i],selecter1]+0.5)*step( t < min(day.exposed[i,j,selecter1], day.infectious.end[i,k.last[i],selecter1]) + 0.5)
+      d[i,j,(N.hh.members[i] + 1),t] <- step( t > day.infectious[i,(N.hh.members[i] + 1),selecter1]+0.5)*step( t < min(day.exposed[i,j,selecter1], day.infectious.end[i,(N.hh.members[i] + 1),selecter1]) + 0.5)
 
       for(k in 1:(N.hh.members[i] + 1)){
-        p[i,j,k,] <- exp(log.p[i,j,k,])
         log.prob_inf_timej[i,j,k] <- step(abs(j-k)-0.5)*log(1-(1-p[i,j,k,day.exposed[i,j,selecter1]])^(d[i,j,k,day.exposed[i,j,selecter1]]))
       }
     }
     
-    prob_uninf_to_timej[i,j] <- exp(sum(log.prob_uninf[i,j,,1:(day.exposed[i,j]-1)]))
+    prob_uninf_to_timej[i,j] <- exp(sum(log.prob_uninf[i,j,,1:(day.exposed[i,j,selecter1]-1)]))
     prob_uninf[i,j] <- exp(sum(log.prob_uninf[i,j,,1:time.study.HH[i,selecter1]]))
     prob_inf_timej[i,j] <- exp(sum(log.prob_inf_timej[i,j,]))
     prob_inf[i,j] <- prob_uninf_to_timej[i,j]* prob_inf_timej[i,j]

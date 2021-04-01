4aaad4dbba6cafeb535a3c8456bfe3f7c6291b2d
Weinberger <daniel.weinberger@yale.edu>
2021-04-01 22:14:35 +0200

    runs!

--- model/chain_binomial_test_separate.R
+++ model/chain_binomial_test_separate.R
@@ -25,7 +25,9 @@ for(i in 1:N.HH){
 
       }
        
-      d[i,j,(N.hh.members[i] + 1),t] <- 1#step( t > day.infectious[i,(N.hh.members[i] + 1),selecter1]+0.5)*step( t < min(day.exposed[i,j,selecter1], day.infectious.end[i,(N.hh.members[i] + 1),selecter1]) + 0.5)
+      d[i,j,(N.hh.members[i]+1),t] <- 1 #step( t > day.infectious[i,(N.hh.members[i] + 1),selecter1]+0.5)*step( t < min(day.exposed[i,j,selecter1], day.infectious.end[i,(N.hh.members[i] + 1),selecter1]) + 0.5)
+      p[i,j,(N.hh.members[i]+1),t] <- exp(log.p[i,j,(N.hh.members[i]+1),t])
+      log.prob_uninf[i,j,(N.hh.members[i]+1),t] <- log((1-p[i,j,(N.hh.members[i]+1),t])^(d[i,j,(N.hh.members[i]+1),t]))
 
 
     }
@@ -33,10 +35,11 @@ for(i in 1:N.HH){
     for(k in 1:(N.hh.members[i])){
         log.prob_inf_timej[i,j,k] <- step(abs(j-k)-0.5)*log(1-(1-p[i,j,k,day.exposed[i,j,selecter1]])^(d[i,j,k,day.exposed[i,j,selecter1]]))
     }
-    log.prob_inf_timej[i,j,N.hh.members[i]+1] <- 1#log(1-(1-p[i,j,N.hh.members[i]+1,day.exposed[i,j,selecter1]])^(d[i,j,N.hh.members[i]+1,day.exposed[i,j,selecter1]]))
+    
+    log.prob_inf_timej[i,j,N.hh.members[i]+1] <- log(1-(1-p[i,j,(N.hh.members[i]+1),day.exposed[i,j,selecter1]])^(d[i,j,(N.hh.members[i]+1),day.exposed[i,j,selecter1]]))
     prob_uninf_to_timej[i,j] <- exp(sum(log.prob_uninf[i,j,1:N.hh.members[i],1:(day.exposed[i,j,selecter1]-1)]))
     prob_uninf[i,j] <- exp(sum(log.prob_uninf[i,j,1:N.hh.members[i],1:time.study.HH[i,selecter1]]))
-    prob_inf_timej[i,j] <- exp(sum(log.prob_inf_timej[i,j,1:N.hh.members[i]+1]))
+    prob_inf_timej[i,j] <- exp(sum(log.prob_inf_timej[i,j,1:(N.hh.members[i]+1)]))
     prob_inf[i,j] <- prob_uninf_to_timej[i,j]* prob_inf_timej[i,j]
     like[i,j] <- y2[i,j]*prob_uninf[i,j] + (1-y2[i,j])*prob_inf[i,j]+ 1e-6  
   }  

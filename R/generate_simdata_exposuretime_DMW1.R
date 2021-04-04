220d753985127b95b971fd4531e8e56bdd94965e
ottiP <ottavia.prunas@edu.unito.it>
2021-04-04 12:07:33 -0400

    update delay distribution

--- R/generate_simdata_exposuretime_DMW1.R
+++ R/generate_simdata_exposuretime_DMW1.R
@@ -1,7 +1,9 @@
+set.seed(1234)
+library(reshape2)
 ###Simulate  people
-gen.hh <- function(idN,  prob.trans.day=0.025, prop.vax1=0.5, prop.vax2=0.5, irr.vax=0.2, IRR.comm=1){
+gen.hh <- function(idN,  prob.trans.day=0.1, prop.vax1=0.5, prop.vax2=0.5, irr.vax=0.2){
   
-  HH.size <- min(1+ rpois(n=1,1.5),5) #cap at 5
+  HH.size <- min(2+ rpois(n=1,1.5),5) #cap at 5
   df1 <- as.data.frame(matrix(NA, nrow=HH.size, ncol=2))
   names(df1) <- c('ID', 'hhID')
   df1$hhID <- idN
@@ -11,33 +13,30 @@ gen.hh <- function(idN,  prob.trans.day=0.025, prop.vax1=0.5, prop.vax2=0.5, irr
   # df1$date.test[1] <- as.Date('2020-12-20')
   # df1$date.test[2:nrow(df1)] <- df1$date.test[1] + 1 + 21*runif(n=(nrow(df1)-1)) 
   # 
-  
   df1$vax1dose <- rbinom(nrow(df1), 1,prop.vax1)
   df1$vax2dose <- rbinom(nrow(df1), 1,prop.vax2)
-  
+
   #Randomly set 1 person as the index
   ##Might need to modify this so that index is unvaccinated person
-  
   rand1 <- runif(nrow(df1))
-  
   df1$index <- 1*(rand1==min(rand1))
-  
+
   #These are time distributions that specify for the person if they are infected, how much time to add before infectious, and ow long infectious
-  expose.dist= rgamma(length(df1$ID),4,  0.75) #duration latent
-  infect.dist= rgamma(length(df1$ID),4, 0.75) #duration infectiousness
+  expose.dist= rgamma(length(df1$ID),1.33,1/3.46) #duration latent
+  infect.dist= rgamma(length(df1$ID),1.33,1/3.46) #duration infectiousness
+
+  exposed.status <- matrix(NA, nrow=nrow(df1), ncol=21)
+  infect.status <- matrix(NA, nrow=nrow(df1), ncol=21)
+  n.infect.prev <- matrix(NA, nrow=nrow(df1), ncol=21)
   
-  exposed.status <- matrix(NA, nrow=nrow(df1), ncol=40)
-  infect.status <- matrix(NA, nrow=nrow(df1), ncol=40)
-  n.infect.prev <- matrix(NA, nrow=nrow(df1), ncol=40)
-  
-  exposed.status[,1] <- df1$index*rbinom(nrow(df1),1, 0.75*IRR.comm^(df1$vax1dose)) #60% chance that a tested index is positive 
+  exposed.status[,1] <- df1$index
   infect.status[,1] <- 0
   n.infect.prev[,1] <- 0
   
   prob.infect.day <- prob.trans.day * irr.vax^df1$vax1dose   #prob of being infected per day, per exposure,
   prob.uninfect.day <- 1 - prob.infect.day
   
-  for( i in 2:40){
+  for( i in 2:21){
     day.exposed <- apply(exposed.status,1, function(x) which(x==1)[1])
     day.exposed[is.na(day.exposed)] <- 0
     
@@ -59,3 +58,22 @@ gen.hh <- function(idN,  prob.trans.day=0.025, prop.vax1=0.5, prop.vax2=0.5, irr
   return(df1)
   
 }
+  
+   
+#Generate the data and store as a data frame
+hh_list <- lapply(1:1000,gen.hh )
+hh_df <- do.call('rbind.data.frame', hh_list)
+
+hh_df.m <- melt(hh_df[,c('day_index','hhID','ID','infected','vax1dose')], id.vars=c('hhID','ID'))
+
+hh_df.c <- acast(hh_df.m, hhID~ ID ~variable )
+
+day.matrix <- hh_df.c[,,'day_index']
+infected_matrix <- hh_df.c[,,'infected']
+vax <- hh_df.c[,,'vax1dose']
+#vax1dose <- hh_df.c[,,'vax1dose']
+#vax2dose <- hh_df.c[,,'vax2dose']
+N.hh.members <- apply(infected_matrix,1,function(x) sum(!is.na(x)))
+N.HH <- nrow(infected_matrix)
+
+N.cases.hh <- apply(infected_matrix,1,sum, na.rm=T)

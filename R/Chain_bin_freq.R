81d63958901e47ef28994bde47cd9cba3e7c1a19
Weinberger <daniel.weinberger@yale.edu>
2021-04-08 17:04:18 +0200

    function to generate the calls

--- R/Chain_bin_freq.R
+++ R/Chain_bin_freq.R
@@ -1,4 +1,5 @@
 library(boot)
+library(pbapply)
 library(matrixStats)
 library(reshape2)
 library(data.table)
@@ -16,72 +17,84 @@ kappa=0.5
 params <- c(alpha0,delta0,beta,kappa)
 
 #Generate the data and store as a data frame
-sim.data.ls <- lapply(1:10,gen.hh)
+sim.data.ls <- pblapply(1:10000,gen.hh)
 
-df1.ls <- lapply(sim.data.ls, delay_dist_sim)
+#This is like the data we would get from KSM
+sim.data.df <- do.call('rbind.data.frame', sim.data.ls)
 
-df1 <- do.call('rbind.data.frame',df1.ls)
+delay.gen <- function(input_df){
+    sim.data.df.spl <- split(input_df, sim.data.df$hhID)
+    
+    df1.ls <- lapply(sim.data.df.spl, delay_dist_sim)
+    
+    df1 <- do.call('rbind.data.frame',df1.ls)
+    
+    #keep just variables we need
+    df1b <- df1[, c('ID','hhID','vax1dose','vax2dose','infected','day.exposed','day.infectious.end','day.infectious','max.time.hh')]
+    
+    ### Create a  copy of df1 and call it df2
+    df2 <- df1b
+    
+    ### Rename all of the variables on df2 as HHID_b, ID_b, Vax_dose1b
+    colnames(df2) <- paste(colnames(df2), 'b', sep='_')
+    
+    ### Merge df1 and df2 by HHID; name this df3 
+    df3 <- merge(df1b,df2,by.y='hhID_b' , by.x='hhID')
+    
+    df3$expand.t <- NA
+    df3$expand.t[df3$infected==0] <- df3$max.time.hh[df3$infected==0] #censor uninfected person at max.time.hh
+    df3$expand.t[df3$infected==1] <- df3$day.exposed[df3$infected==1] #censor infected person at (latent) day of exposure
+    
+    df3$rowN <- 1:nrow(df3)
+    df.t <- df3[rep(df3$rowN, times=df3$expand.t),] #expands the df to number of time points
+    
+    #Add an index for each person
+    df.t$t.index <- unlist(lapply(df3$expand.t, function(x){ 
+      z <- 1:x 
+      return(z)
+      }))
+    
+    # keep rows where the time index is within range of the infectious period for person b;
+    # or where ID_a==ID_b (community risk)
+    df.t$keep <- (df.t$ID == df.t$ID_b) | 
+      (
+        (df.t$t.index >= df.t$day.infectious_b & df.t$t.index <= df.t$day.infectious.end_b & df.t$t.index <= df.t$day.exposed) &
+        (df.t$t.index <= df.t$day.exposed)  
+      )
+    
+    df4 <- df.t[df.t$keep==1,]
+    
+    #Create elements for design matrix
+    df4$delta0 <- 0
+    df4$delta0[(df4$ID == df4$ID_b)] <- 1 #exogenous/community risk 
+    
+    df4$alpha0 <- 0
+    df4$alpha0[df4$ID != df4$ID_b ] <- 1
+    
+    df4$vax1 <- df4$vax1dose
+    
+    df4$vax2 <- df4$vax1dose_b
+    df4$vax2[df4$alpha0==0] <- 0
+    
+    #HMM is this right? seems like we could double-count Y=1 for certain contact pairs?
+    
+    df4$Y <- df4$infected*(df4$day.exposed==df4$t.index)
+    
+    #Design matrix
+    X <- df4[c('alpha0','delta0','vax1','vax2')]
+    
+    Y <- 1 - df4$infected
 
-#keep just variables we need
-df1b <- df1[, c('ID','hhID','vax1dose','vax2dose','infected','day.exposed','day.infectious.end','day.infectious','max.time.hh')]
-
-### Create a  copy of df1 and call it df2
-df2 <- df1b
-
-### Rename all of the variables on df2 as HHID_b, ID_b, Vax_dose1b
-colnames(df2) <- paste(colnames(df2), 'b', sep='_')
-
-### Merge df1 and df2 by HHID; name this df3 
-df3 <- merge(df1b,df2,by.y='hhID_b' , by.x='hhID')
-
-df3$expand.t <- NA
-df3$expand.t[df3$infected==0] <- df3$max.time.hh[df3$infected==0] #censor uninfected person at max.time.hh
-df3$expand.t[df3$infected==1] <- df3$day.exposed[df3$infected==1] #censor infected person at (latent) day of exposure
-
-df3$rowN <- 1:nrow(df3)
-df.t <- df3[rep(df3$rowN, times=df3$expand.t),] #expands the df to number of time points
-
-#Add an index for each person
-df.t$t.index <- unlist(lapply(df3$expand.t, function(x){ 
-  z <- 1:x 
-  return(z)
-  }))
-
-# keep rows where the time index is within range of the infectious period for person b;
-# or where ID_a==ID_b (community risk)
-df.t$keep <- (df.t$ID == df.t$ID_b) | 
-  (
-    (df.t$t.index >= df.t$day.infectious_b & df.t$t.index <= df.t$day.infectious.end_b & df.t$t.index <= df.t$day.exposed) &
-    (df.t$t.index <= df.t$day.exposed)  
-  )
-
-df4 <- df.t[df.t$keep==1,]
-
-#Create elements for design matrix
-df4$delta0 <- 0
-df4$delta0[(df4$ID == df4$ID_b)] <- 1 #exogenous/community risk 
-
-df4$alpha0 <- 0
-df4$alpha0[df4$ID != df4$ID_b ] <- 1
-
-df4$vax1 <- df4$vax1dose
-
-df4$vax2 <- df4$vax1dose_b
-df4$vax2[df4$alpha0==0] <- 0
-
-#HMM is this right? seems like we could double-count Y=1 for certain contact pairs?
-
-df4$Y <- df4$infected*(df4$day.exposed==df4$t.index)
-
-#Design matrix
-X <- df4[c('alpha0','delta0','vax1','vax2')]
-
-Y <- 1 - df4$infected
+  out.list=list('Y'=Y, 'X'=X)    
+    return(out.list)
+}
 
 
 
-  
+delay.gen <- delay.gen(sim.data.df)
 
+
+#Note here, we have all time points represented in the df, so the likelihood is very simple--no need to exponentiate stuff  
 chain_bin_lik <- function(params){
   ll=0
 

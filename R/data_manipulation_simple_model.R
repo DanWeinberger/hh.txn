7a50948a1192e2c62b30e5f78ecec5e98a7734d7
ottiP <ottavia.prunas@edu.unito.it>
2021-04-19 19:47:36 -0400

    Run simple model

---
+++ R/data_manipulation_simple_model.R
@@ -0,0 +1,75 @@
+library(lme4)
+delay.gen.simplified <- function(input_df){
+  sim.data.df.spl <- split(input_df, input_df$hhID)
+  
+  df1.ls <- lapply(sim.data.df.spl, delay_dist_sim)
+  
+  df1 <- do.call('rbind.data.frame',df1.ls)
+  
+  #keep just variables we need
+  df1b <- df1[, c('ID','hhID','vax1dose','vax2dose','infected','day.exposed','day.infectious.end','day.infectious','max.time.hh')]
+  
+  ### Create a  copy of df1 and call it df2
+  df2 <- df1b
+  
+  ### Rename all of the variables on df2 as HHID_b, ID_b, Vax_dose1b
+  colnames(df2) <- paste(colnames(df2), 'b', sep='_')
+  
+  ### Merge df1 and df2 by HHID; name this df3 
+  df3 <- merge(df1b,df2,by.y='hhID_b' , by.x='hhID')
+  
+  df3$expand.t <- NA
+  df3$expand.t[df3$infected==0] <- df3$max.time.hh[df3$infected==0] #censor uninfected person at max.time.hh
+  df3$expand.t[df3$infected==1] <- df3$day.exposed[df3$infected==1] #censor infected person at (latent) day of exposure
+  
+  df3$rowN <- 1:nrow(df3)
+  df.t <- df3[rep(df3$rowN, times=df3$expand.t),] #expands the df to number of time points
+  
+  #Add an index for each person
+  df.t$t.index <- unlist(lapply(df3$expand.t, function(x){ 
+    z <- 1:x 
+    return(z)
+  }))
+  
+  df.t$keep <-  T *ifelse(df.t$ID == df.t$ID_b,1,0)  | (df.t$t.index >= df.t$day.infectious_b & df.t$t.index <= df.t$day.infectious.end_b )
+  
+  
+  df4 <- df.t[df.t$keep==1,]
+  
+  #Create elements for design matrix
+  df4$delta0 <- 0
+  df4$delta0[(df4$ID == df4$ID_b)] <- 1 #exogenous/community risk 
+  
+  df4$alpha0 <- 0
+  df4$alpha0[df4$ID != df4$ID_b ] <- 1
+  
+  df4$vax1 <- df4$vax1dose
+  
+  df4$vax2 <- df4$vax1dose_b
+  df4$vax2[df4$delta0==1] <- 0 #person B's vaccine status doesn't influence whether person A is infected
+  
+  df4$infect.at.timet <- df4$infected
+  df4$infect.at.timet[df4$t.index < df4$day.exposed] <- 0
+  
+  #Y.df <- aggregate( df4$infect.at.timet, by=list( 'ID'=df4$ID, 'hhID'=df4$hhID, 'tindex'=df4$t.index ), FUN=mean)
+  data_tab <- cbind.data.frame(df4[,c('infect.at.timet','ID','hhID','t.index','vax1dose')])
+  data_t = data.table(data_tab)
+  Y.df = data_t[,list(A = mean(infect.at.timet)), by = 'ID,hhID,t.index,vax1dose']
+  Y.df <- setorder(Y.df, hhID, ID, t.index,vax1dose) ### To order based on t.index and not ID
+  
+  
+  #Design matrix 
+  X <- df4[c('alpha0','delta0','vax1','vax2','ID','hhID','t.index')]  ###CHECK if OK   
+  X <- data.table(X)
+  X <- setorder(X,  hhID, ID, t.index)
+  
+  Y <-  Y.df$A
+  
+  out.list=list('Y'=Y, 'Y.df'=Y.df)  
+  return(out.list)
+}
+
+
+
+
+
---
+++ R/simple_model_run.R
@@ -0,0 +1,42 @@
+library(boot)
+library(pbapply)
+library(matrixStats)
+library(reshape2)
+library(data.table)
+library(stats4)
+library(lme4)
+
+
+source('./R/simulate_data.R')
+source('./R/delay_dist_sim.R')
+source('./R/data_manipulation_simple_model.R')
+
+
+################ Define glmer function
+#Generate the synthetic data and store as a data frame
+N.HH <- 5000
+sim.data.ls <- pblapply(1:N.HH, gen.hh,CPI=(1-0.9995), #Increase CPI to have more cases, 
+                        prob.trans.day=(1-0.968),
+                        irr.vax1=0.2,irr.vax2=1)
+
+#This is like the data we would get from KSM
+sim.data.df <- do.call('rbind.data.frame', sim.data.ls)
+
+
+LatentData <- delay.gen.simplified(sim.data.df)
+Y <- LatentData$Y
+X <- LatentData$Y.df
+colnames(X)[5]<-'Y'
+
+X <- data.frame(X)
+outcome_name <- 'Y'
+
+formula <- as.formula(paste0(outcome_name, '~',
+                             'ID+ hhID+vax1dose+(1|hhID)'))
+mod1 <- glmer(formula = formula, data=X, family=binomial(link="cloglog"),verbose=TRUE)
+
+
+summary(mod1)
+
+
+

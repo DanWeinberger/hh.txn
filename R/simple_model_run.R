c487f27b46b7ad3df177c5c6c8c20a3f0c16902d
ottiP <ottavia.prunas@edu.unito.it>
2021-04-21 20:46:28 -0400

    small changes

--- R/simple_model_run.R
+++ R/simple_model_run.R
@@ -14,7 +14,7 @@ source('./R/data_manipulation_simple_model.R')
 
 ################ Define glmer function
 #Generate the synthetic data and store as a data frame
-N.HH <- 10
+N.HH <- 50
 sim.data.ls <- pblapply(1:N.HH, gen.hh,CPI=(1-0.9995), #Increase CPI to have more cases, 
                         prob.trans.day=(1-0.968),
                         irr.vax1=0.2,irr.vax2=1)
@@ -25,8 +25,7 @@ sim.data.df <- do.call('rbind.data.frame', sim.data.ls)
 input_df <- sim.data.df
 LatentData <- delay.gen.simplified(sim.data.df)
 Y <- LatentData$Y
-X <- LatentData$Y.df
-colnames(X)[5]<-'Y'
+X <- LatentData$X.df
 
 X <- data.frame(X)
 outcome_name <- 'Y'

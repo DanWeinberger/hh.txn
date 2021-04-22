c5173e2189bf4b5f58f2d95213b2c8d6a021fda3
ottiP <ottavia.prunas@edu.unito.it>
2021-04-21 20:46:05 -0400

    Update simplified model

--- R/data_manipulation_simple_model.R
+++ R/data_manipulation_simple_model.R
@@ -3,9 +3,15 @@ delay.gen.simplified <- function(input_df){
   sim.data.df.spl <- split(input_df, input_df$hhID)
   
   df1.ls <- lapply(sim.data.df.spl, delay_dist_sim)
-  
   df1 <- do.call('rbind.data.frame',df1.ls)
-  
+ # df1 <- df1[1:4,]
+ # df1$ID <- c(1,2,3,4)
+ # df1$hhID <- c(1,1,1,1)
+ # df1$infected <- c(1,1,0,0)
+ # df1$day.infectious  <- c(3,6,4,9)
+ # df1$day.exposed <- c(2,4,3,6)
+ # df1$day.infectious.end <- c(6,9,8,11)
+ # df1$max.time.hh <- c(11,11,11,11)
   #keep just variables we need
   df1b <- df1[, c('ID','hhID','vax1dose','vax2dose','infected','day.exposed','day.infectious.end','day.infectious','max.time.hh')]
   
@@ -31,7 +37,7 @@ delay.gen.simplified <- function(input_df){
     return(z)
   }))
   
-  df.t$keep <-  T *ifelse(df.t$ID == df.t$ID_b,1,0)  | (df.t$t.index >= df.t$day.infectious_b & df.t$t.index <= df.t$day.infectious.end_b )
+  df.t$keep <-  T *ifelse(df.t$ID == df.t$ID_b,1,0)  | (df.t$t.index >= df.t$day.infectious_b & df.t$t.index <= df.t$day.infectious.end_b )*ifelse(df.t$infected_b==1,1,0)
   
   
   df4 <- df.t[df.t$keep==1,]
@@ -51,21 +57,68 @@ delay.gen.simplified <- function(input_df){
   df4$infect.at.timet <- df4$infected
   df4$infect.at.timet[df4$t.index < df4$day.exposed] <- 0
   
+  
+  df4$atrisk <- 0
+  df4$atrisk[(df4$t.index >= df4$day.infectious_b & df4$t.index <= df4$day.infectious.end_b )  & (df4$infected_b==1)] <- 1
+  
+  df4$m <- 0
+  df4$m[(df4$atrisk==1) &  (df4$vax1dose_b!=1)] <- 1
+  
+  df4$z <- 0
+  df4$z[(df4$atrisk==1) &  (df4$vax1dose_b==1)] <- 1
+  
+  
+  data_tab.X <- cbind.data.frame(df4[,c('m','ID','hhID','t.index','vax1dose')])
+  data_t.X = data.table(data_tab.X)
+  X.df = data_t.X[,list(A = sum(m)), by = 'ID,hhID,t.index,vax1dose']
+  colnames(X.df)[ncol(X.df)]  <- 'm'
+  
+  data_tab.X.z <- cbind.data.frame(df4[,c('z','ID','hhID','t.index','vax1dose')])
+  data_t.X.z = data.table(data_tab.X.z)
+  X.df.z = data_t.X.z[,list(A = sum(z)), by = 'ID,hhID,t.index,vax1dose']
+  colnames(X.df.z)[ncol(X.df.z)]  <- 'z'
+  
+  
+  X.df$z <-  X.df.z$z
+  
   #Y.df <- aggregate( df4$infect.at.timet, by=list( 'ID'=df4$ID, 'hhID'=df4$hhID, 'tindex'=df4$t.index ), FUN=mean)
-  data_tab <- cbind.data.frame(df4[,c('infect.at.timet','ID','hhID','t.index','vax1dose')])
-  data_t = data.table(data_tab)
-  Y.df = data_t[,list(A = mean(infect.at.timet)), by = 'ID,hhID,t.index,vax1dose']
+  data_tab.Y <- cbind.data.frame(df4[,c('infect.at.timet','ID','hhID','t.index','vax1dose')])
+  data_t.Y = data.table(data_tab.Y)
+  Y.df = data_t.Y[,list(A = mean(infect.at.timet)), by = 'ID,hhID,t.index,vax1dose']
+  #Y.df = data_t[,list(A = mean(infect.at.timet)), by = 'ID,hhID,t.index,vax1dose,infected_b']
+  
   Y.df <- setorder(Y.df, hhID, ID, t.index,vax1dose) ### To order based on t.index and not ID
   
+
   
-  #Design matrix 
-  X <- df4[c('alpha0','delta0','vax1','vax2','ID','hhID','t.index')]  ###CHECK if OK   
-  X <- data.table(X)
-  X <- setorder(X,  hhID, ID, t.index)
+  #Y.df$m <- 0
+  #Y.df$z <- 0
+  #df4.agg.z <- df4[(df4$vax1dose_b==1) &(df4$t.index>=df4$day.infectious_b) & (df4$t.index<=df4$day.infectious.end_b) & (df4$infected_b==1),]
+  #df4.agg.m <- df4[(df4$vax1dose_b!=1) &(df4$t.index>=df4$day.infectious_b) & (df4$t.index<=df4$day.infectious.end_b) & (df4$infected_b==1),]
   
+  #df4.agg.m <- df4[(df4$vax1dose_b!=1),]
+  #agg.m <- aggregate(df4.agg.m$ID_b, by=list('ID'=df4.agg.m$ID, 'hhID'=df4.agg.m$hhID, 'tindex'=df4.agg.m$t.index ), FUN=length)
+  
+ 
+# 
+# 
+#   if(nrow(df4.agg.m)!=0){
+#     agg.m <- aggregate(df4.agg.m$ID_b, by=list('ID'=df4.agg.m$ID, 'hhID'=df4.agg.m$hhID, 'tindex'=df4.agg.m$t.index ), FUN=length)
+#     #agg2 <- 0
+#     Y.df[(Y.df$ID %in% agg.m$ID) & (Y.df$hhID %in% agg.m$hhID) &  (Y.df$t.index %in% agg.m$tindex) & (Y.df$t.index>=Y.df$day.infectious_b) & (Y.df$t.index<= Y.df$day.infectious.end_b),]$m <- agg.m$x
+#   }
+#   
+#   if(nrow(df4.agg.z)!=0){
+#     agg.z <- aggregate(df4.agg.z$ID_b, by=list('ID'=df4.agg.z$ID, 'hhID'=df4.agg.z$hhID, 'tindex'=df4.agg.z$t.index ), FUN=length)
+#     Y.df[(Y.df$ID %in% agg.z$ID) & (Y.df$hhID %in% agg.z$hhID) &  (Y.df$t.index %in% agg.z$tindex) & (Y.df$t.index>=Y.df$day.infectious_b) & (Y.df$t.index<= Y.df$day.infectious.end_b),]$z <- agg.z$x
+#     
+#   }
+
   Y <-  Y.df$A
   
-  out.list=list('Y'=Y, 'Y.df'=Y.df)  
+  X.df$Y <- Y
+  
+  out.list=list( 'X.df'=X.df)  
   return(out.list)
 }
 
--- R/simple_model_run.R
+++ R/simple_model_run.R
@@ -14,7 +14,7 @@ source('./R/data_manipulation_simple_model.R')
 
 ################ Define glmer function
 #Generate the synthetic data and store as a data frame
-N.HH <- 5000
+N.HH <- 10
 sim.data.ls <- pblapply(1:N.HH, gen.hh,CPI=(1-0.9995), #Increase CPI to have more cases, 
                         prob.trans.day=(1-0.968),
                         irr.vax1=0.2,irr.vax2=1)
@@ -22,7 +22,7 @@ sim.data.ls <- pblapply(1:N.HH, gen.hh,CPI=(1-0.9995), #Increase CPI to have mor
 #This is like the data we would get from KSM
 sim.data.df <- do.call('rbind.data.frame', sim.data.ls)
 
-
+input_df <- sim.data.df
 LatentData <- delay.gen.simplified(sim.data.df)
 Y <- LatentData$Y
 X <- LatentData$Y.df
@@ -32,7 +32,7 @@ X <- data.frame(X)
 outcome_name <- 'Y'
 
 formula <- as.formula(paste0(outcome_name, '~',
-                             'vax1dose+(1|hhID)'))
+                             'vax1dose+m+z+(1|hhID)'))
 mod1 <- glmer(formula = formula, data=X, family=binomial(link="cloglog"),verbose=TRUE)
 
 
